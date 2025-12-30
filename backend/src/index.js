5c9fcc25a2015f919e5dc8a2ac90bea7374d9cf8
tomas <tomasmcgrath21@gmail.com>
2025-12-30 17:03:24 +0000

    Add CORs for backend routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import wordsRouter from './api/words';
 
 
+
 dotenv.config();
 const app = express() 
 const port = process.env.port || 3000;
+
+app.use(cors());
+
 
 
 
