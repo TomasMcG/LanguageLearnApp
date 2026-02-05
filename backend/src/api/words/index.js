7bc2db263d3006b8c9a2162ebb929e4be243fcf4
tomas <tomasmcgrath21@gmail.com>
2026-02-05 00:21:52 +0000

    Connect MongoDB and get words from it

--- backend/src/api/words/index.js
+++ backend/src/api/words/index.js
@@ -1,10 +1,21 @@
-import express from 'express';
-import { wordData } from "./wordData.js";
+import express from "express";
+//import { wordData } from "./wordData.js";
+import Word from "./wordModel.js";
 
 const router = express.Router();
-
+/*
 router.get('/', (req, res) => {
   res.json(wordData);
+});*/
+
+router.get("/", async (req, res) => {
+  try {
+    const words = await Word.find();
+    console.log("WORDS FROM DB:", words);
+    res.status(200).json(words);
+  } catch (err) {
+    res.status(500).json([]);
+  }
 });
 
 export default router;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,22 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import wordsRouter from './api/words/index.js';
-import { wordData } from './api/words/wordData.js';
-
-
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import "../db/index.js";
+import wordsRouter from "./api/words/index.js";
+import { wordData } from "./api/words/wordData.js";
 
 dotenv.config();
-const app = express() 
+const app = express();
 const port = process.env.port || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-
-
-app.get('/', (req, res) => {
-  res.send(wordData)
-})
-app.use('/api/words',wordsRouter);
-
-
-
+app.get("/", (req, res) => {
+  res.send(wordData);
+});
+app.use("/api/words", wordsRouter);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
