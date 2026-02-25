d2f70a5bfbf7c38db6f9b3f43866a5226d909e6b
tomas <tomasmcgrath21@gmail.com>
2026-02-25 20:52:04 +0000

    Add quick login button for testing

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,53 +1,67 @@
-import { styles } from '@/styles/inputStyles';
-import { useRouter } from 'expo-router';
+import { styles } from "@/styles/inputStyles";
+import { useRouter } from "expo-router";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 import { auth } from "../../firebase";
-
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("Please Login")
+  const [message, setMessage] = useState("Please Login");
 
-const router = useRouter();
+  const router = useRouter();
 
 
- 
+  const quickEmail = process.env.EXPO_PUBLIC_QUICK_LOGIN_EMAIL ?? "0";
+    const quickPass = process.env.EXPO_PUBLIC_QUICK_LOGIN_PASSWORD ?? "0";
 
   const login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         setMessage(`Logged in: ${userCredential.user.email}`);
-        router.replace("/(tabs)/explore")
+        router.replace("/(tabs)/explore");
       })
       .catch((error) => {
         setMessage(`Login error: ${error.message}`);
       });
   };
 
-  
+  const quickLogin = () => {
+    signInWithEmailAndPassword(auth, quickEmail, quickPass)
+      .then((userCredential) => {
+        setMessage(`Logged in: ${userCredential.user.email}`);
+        router.replace("/(tabs)/explore");
+      })
+      .catch((error) => {
+        setMessage(`Login error: ${error.message}`);
+      });
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.TextHeader}> Email:</Text>
-      <TextInput style={styles.TextInput}
+      <TextInput
+        style={styles.TextInput}
         value={email}
         onChangeText={setEmail}
-        
       />
 
       <Text style={styles.TextHeader}>Password:</Text>
-      <TextInput style={styles.TextInput}
+      <TextInput
+        style={styles.TextInput}
         value={password}
         onChangeText={setPassword}
-       
       />
-       <Button title="Login" onPress={login} />
-       <Text style={styles.TextHeader}>{message}</Text>
+      <Button title="Login" onPress={login} />
+      <Text style={styles.TextHeader}>{message}</Text>
+
+      <View>
+        <Button title="Qucik Login" onPress={quickLogin} />
+        <Text>
+          {quickEmail},{quickPass}
+        </Text>
       </View>
+    </View>
   );
 }
-
-
--- backend/src/api/userWords/userWordModel.js
+++ backend/src/api/userWords/userWordModel.js
@@ -5,9 +5,9 @@ const Schema = mongoose.Schema;
 const WordSchema = new Schema({
   wordId: { type: mongoose.Schema.Types.ObjectId, required: true },
   userId: { type: String, required: true },
-  proficiencyLevel: { type: Number, required: true },
+  proficiencyLevel: { type: Number },
   lastReviewed: { type: Date, default: Date.now},
-  timeToNextReview: { type: Number, required: true },
+  timeToNextReview: { type: Number, default:0 },
   nextReviewDate: {type: Date, default: Date.now},
   isKnown: { type: Number, required: true },
 });
