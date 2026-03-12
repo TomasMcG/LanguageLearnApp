a25b3d24e208eb793ecf1f226a2091a7a8d81b3c
tomas <tomasmcgrath21@gmail.com>
2026-03-12 11:36:07 +0000

    Improve AI prompt

--- backend/AI_Test.js
+++ backend/AI_Test.js
@@ -3,10 +3,28 @@ import dotenv from "dotenv";
 dotenv.config();
 const client = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
+const systemMessage = `
+You are a language teacher.
+Generate English and German sentences only using the words provided.
+Adhere to provided grammer rules for sentence complexity.
+You can add basic words to fill out the sentences if unable to form with just the words given.
+Return JSON: sentence, translation.
+`;
+
+const grammerRules = ["PastTense,PresentTense","VerbEndings"]
+const knownWords = ["apfel","Banane","Rot","Haus"]
+const userMessage = `
+Generate 5 Spanish sentences using only these words: ${knownWords} and grammer rules: ${grammerRules}
+`;
+
 const response = await client.responses.create({
-    model: "gpt-5-mini",
-    input: `Generate 5 beginner german sentences with the  words apple,red,student,house,family`
-    
+  model: "gpt-5-mini",
+  input: [
+    { role: "system", content: systemMessage },
+    { role: "user", content: userMessage }
+  ]
 });
+
+
 
 console.log(response.output_text);
