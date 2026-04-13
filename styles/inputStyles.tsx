de1ec30773655c292b8c30becbce18edaa59d920
tomas <tomasmcgrath21@gmail.com>
2026-04-13 21:38:37 +0100

    More Styling

--- styles/inputStyles.tsx
+++ styles/inputStyles.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: {
+  padding: 16,
+  backgroundColor: "#f2f2f2",
+},
   text: { fontSize: 24, fontWeight: 'bold' },
   TextInput: {width: "50%",
     borderWidth: 1,
